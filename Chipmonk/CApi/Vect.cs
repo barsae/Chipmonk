244c489c97c590c55b765d944f74b8ab8a68e8a4
ebarsalou <barsae@gmail.com>
2016-05-08 22:18:03 -0700

    New: Add a couple of utility functions to Vect

--- Chipmonk/CApi/Vect.cs
+++ Chipmonk/CApi/Vect.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace Chipmonk.CApi {
     public struct Vect {
         public double X;
@@ -11,6 +12,22 @@ namespace Chipmonk.CApi {
 
         public override string ToString() {
             return string.Format("({0}, {1})", X, Y);
+        }
+
+        public double Length() {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+
+        public static Vect operator*(Vect vect, double d) {
+            return new Vect(vect.X * d, vect.Y * d);
+        }
+
+        public static Vect operator/(Vect vect, double d) {
+            return new Vect(vect.X / d, vect.Y / d);
+        }
+
+        public static Vect operator-(Vect vect) {
+            return new Vect(-vect.X, -vect.Y);
         }
 
         public static Vect Zero = new Vect(0.0, 0.0);
